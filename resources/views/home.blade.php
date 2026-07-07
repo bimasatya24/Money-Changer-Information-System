0685aa0598fa48d21c3951bc3c8ca89655efb665
bimasatya24 <bimasatyanugraha@gmail.com>
2026-07-07 13:56:16 +0700

    Menambahkan informasi halaman utama dengan kode html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,38 @@
     <title>Money Changer</title>
 </head>
 <body>
-    
+    <header>
+        <div>
+            <h1>PT Bina Sukses Valasindo</h1>
+            <p>Layanan Penukaran Valuta Asing Terpecaya</p>
+        </div>
+    </header>
+    <div>
+        <h2>Kurs Hari Ini</h2>
+        <div>
+            <p>Terakhir Diperbarui: </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>MATA UANG</th>
+                        <th>PECAHAN</th>
+                        <th>BELI</th>
+                        <th>JUAL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr></tr>
+                </tbody>
+            </table>
+            <div>
+                <b>HARGA SEWAKTU-WAKTU DAPAT BERUBAH</b>
+                <br>
+                <b>UNTUK KETERSEDIAAN STOK KONFIRMASI TERLEBIH DAHULU!</b>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2026 PT Bina Sukses Valasindo. All rights reserved.</p>
+    </footer>
 </body>
 </html>
