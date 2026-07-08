8a3d871994a0fc1184dc43e7fdbb8f80408dc705
bimasatya24 <bimasatyanugraha@gmail.com>
2026-07-08 12:00:14 +0700

    Memuat tampilan tailwind css part 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,40 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Money Changer</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>PT Bina Sukses Valasindo</title>
 </head>
 <body>
-    <header>
-        <div>
-            <h1>PT Bina Sukses Valasindo</h1>
-            <p>Layanan Penukaran Valuta Asing Terpecaya</p>
+    <header class="bg-blue-600 text-white py-3 mb-4">
+        <div class="container text-center">
+            <h1 class="text-5xl">PT Bina Sukses Valasindo</h1>
+            <p class="text-xl">Layanan Penukaran Valuta Asing Terpecaya</p>
         </div>
     </header>
-    <div>
-        <h2>Kurs Hari Ini</h2>
+    <div class="container">
+        <h2 class="text-center mb-4 text-4xl">Kurs Hari Ini</h2>
         <div>
-            <p>Terakhir Diperbarui: </p>
-            <table>
-                <thead>
-                    <tr>
-                        <th>MATA UANG</th>
-                        <th>PECAHAN</th>
-                        <th>BELI</th>
-                        <th>JUAL</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr></tr>
-                </tbody>
-            </table>
-            <div>
+            <p class="text-2xl font-bold text-right mb-4">Terakhir diperbarui : </p>
+            <div class="flex justify-center">
+                <table class="table border-collapse text-center mb-4">
+                    <thead>
+                        <tr class="text-4xl">
+                            <th class="bg-[rgb(220,53,69)] text-white">MATA UANG</th>
+                            <th class="bg-[rgb(118,117,125)] text-white">PECAHAN</th>
+                            <th class="bg-[rgb(255,193,7)] p-1">BELI</th>
+                            <th class="bg-[rgb(25,135,84)] p-2">JUAL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-4xl">
+                            <td>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="text-center text-4xl text-[rgb(220,53,69)]">
                 <b>HARGA SEWAKTU-WAKTU DAPAT BERUBAH</b>
                 <br>
                 <b>UNTUK KETERSEDIAAN STOK KONFIRMASI TERLEBIH DAHULU!</b>
             </div>
         </div>
     </div>
-    <footer>
+    <footer class="bg-black text-white text-center py-3 mt-4">
         <p>&copy; 2026 PT Bina Sukses Valasindo. All rights reserved.</p>
     </footer>
 </body>
